4a152be1b554ed1bf11739b8a498b66f807888f7
Bao Vien Ngo <baovienngo@gmail.com>
2024-10-27 00:09:57 +0200

    Change base to vien.ngo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**.*.jpg", "**/*.otf", "**/*.png"],
-  base: "/azolive-web/",
+  base: "/vien.ngo/",
 });
